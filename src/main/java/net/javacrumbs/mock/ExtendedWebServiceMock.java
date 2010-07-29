7d5a34f3d727475ff54b560fbdd35701aa0408d3
Lukas Krecan <lukas@krecan.net>
2010-07-29 13:00:01 +0200

    Message comparison added

---
+++ src/main/java/net/javacrumbs/mock/ExtendedWebServiceMock.java
@@ -0,0 +1,39 @@
+package net.javacrumbs.mock;
+import javax.xml.transform.Source;
+
+import org.springframework.util.Assert;
+import org.springframework.ws.mock.client.RequestMatcher;
+import org.springframework.ws.mock.client.WebServiceMock;
+
+/*
+ * Copyright 2005-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Adds extra features to {@link WebServiceMock}. 
+ */
+public abstract class ExtendedWebServiceMock  {
+    /**
+     * Expects the given {@link Source} XML message. Message can either be whole SOAP message or just a payload.
+     * If only payload is passed in, only payloads will be compared, otherwise whole message will be compared.
+     *
+     * @param message the XML message
+     * @return the request matcher
+     */
+    public static RequestMatcher message(Source message) {
+        Assert.notNull(message, "'message' must not be null");
+        return new MessageDiffMatcher();
+    }	
+}
