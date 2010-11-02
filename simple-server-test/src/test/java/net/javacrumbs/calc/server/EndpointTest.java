725d57dc248dcd73c26829dc5cf72d48f81536f4
Lukas Krecan <lukas@krecan.net>
2010-11-02 22:06:28 +0100

    Endpoint test updated

--- simple-server-test/src/test/java/net/javacrumbs/calc/server/EndpointTest.java
+++ simple-server-test/src/test/java/net/javacrumbs/calc/server/EndpointTest.java
@@ -57,26 +57,15 @@ public class EndpointTest {
 	public void testError() throws Exception {
 		wsMockClient.sendMessage(withContent("request-error.xml")).andExpect(message("response-error.xml"));
 	}
-//	@Test
-//	public void testErrorMessage() throws Exception {
-//		// simulates request coming to MessageDispatcherServlet
-//		MessageContext message = helper.receiveMessage("request-error.xml");
-//		// compare response with control response
-//		helper.createMessageValidator(message.getResponse()).assertSoapFault().assertFaultStringOrReason("Validation error").assertContain("'aaa' is not a valid value for 'integer'").assertContainElement("faultstring");
-//	}
-//
-//	@Test
-//	public void testResponseTemplate() throws Exception {
-//		WsTestContextHolder.getTestContext().setAttribute("a", 1);
-//		WsTestContextHolder.getTestContext().setAttribute("b", 2);
-//		WsTestContextHolder.getTestContext().setAttribute("result", 3);
-//		// simulates request coming to MessageDispatcherServlet
-//		MessageContext message = helper.receiveMessage("request-context-xslt.xml");
-//		// compare response with control response
-//		helper.createMessageValidator( message.getResponse()).assertNotSoapFault().compare("response-context-xslt.xml");
-//		
-//		WsTestContextHolder.getTestContext().clear();
-//	}
+	@Test
+	public void testErrorMessage() throws Exception {
+		wsMockClient.sendMessage(withContent("request-error.xml")).andExpect(clientOrSenderFault("Validation error"));
+	}
+
+	@Test
+	public void testResponseTemplate() throws Exception {
+		wsMockClient.sendMessage(withContent("request-context-xslt.xml").withParameter("a",1).withParameter("b", 2)).andExpect(message("response-context-xslt.xml").withParameter("result", 3));
+	}
 //	
 //	@Test
 //	public void testAssertXPathContext() throws Exception {
