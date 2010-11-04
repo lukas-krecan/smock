8b1c37d95dd1ffc0cfdd615f4b31dc6b8c4c2714
Lukas Krecan <lukas@krecan.net>
2010-11-04 20:56:15 +0100

    Test enhanced

--- simple-server-test/src/test/java/net/javacrumbs/calc/server/EndpointTest.java
+++ simple-server-test/src/test/java/net/javacrumbs/calc/server/EndpointTest.java
@@ -4,6 +4,9 @@ import static net.javacrumbs.smock.server.SmockServer.*;
 import static org.springframework.ws.test.server.MockWebServiceClient.*;
 import static org.springframework.ws.test.server.ResponseMatchers.*;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +16,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ws.test.server.MockWebServiceClient;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-// your endpoint configuration + Default helper config
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring-ws-servlet.xml"})
 public class EndpointTest {
 
 	
+	private static final Map<String, String> NS_MAP = Collections.singletonMap("ns", "http://javacrumbs.net/calc");
 	private MockWebServiceClient wsMockClient;
 	
 	@Autowired
@@ -29,30 +32,22 @@ public class EndpointTest {
 	@Test
 	public void testSimple() throws Exception {
 		// simulates request coming to MessageDispatcherServlet
-		wsMockClient.sendMessage(withContent("request1.xml")).andExpect(anything());
+		wsMockClient.sendMessage(withContent("request1.xml")).andExpect(noFault());
 	}
 	
 	@Test
 	public void testCompare() throws Exception {
-		wsMockClient.sendMessage(withContent("request1.xml")).andExpect(payload(fromResource("response1.xml")));
+		wsMockClient.sendMessage(withContent("request1.xml")).andExpect(message("response1.xml"));
 	}
-//	@Test
-//	public void testValidateResponse() throws Exception {
-//		wsMockClient.sendMessage(withContent("request1.xml")).andExpect();
-//		// simulates request coming to MessageDispatcherServlet
-//		MessageContext message = helper.receiveMessage("request1.xml");
-//		//validate response
-//		helper.createMessageValidator(message.getResponse()).assertNotSoapFault().validate("xsd/calc.xsd").compare("response1.xml");
-//				
-//	}
-//	@Test
-//	public void testAssertXPath() throws Exception {
-//		// simulates request coming to MessageDispatcherServlet
-//		MessageContext message = helper.receiveMessage("request1.xml");
-//		//validate response
-//		helper.createMessageValidator(message.getResponse()).assertNotSoapFault().validate("xsd/calc.xsd").addNamespaceMapping("ns", "http://javacrumbs.net/calc").assertXPath("//ns:result=3");
-//	}
-//
+	@Test
+	public void testValidateResponse() throws Exception {
+		wsMockClient.sendMessage(withContent("request1.xml")).andExpect(noFault()).andExpect(validPayload(resource("xsd/calc.xsd")));
+	}
+	@Test
+	public void testAssertXPath() throws Exception {
+		wsMockClient.sendMessage(withContent("request1.xml")).andExpect(noFault()).andExpect(xpath("//ns:result",NS_MAP).evaluatesTo(3));
+	}
+
 	@Test
 	public void testError() throws Exception {
 		wsMockClient.sendMessage(withContent("request-error.xml")).andExpect(message("response-error.xml"));
@@ -66,27 +61,4 @@ public class EndpointTest {
 	public void testResponseTemplate() throws Exception {
 		wsMockClient.sendMessage(withContent("request-context-xslt.xml").withParameter("a",1).withParameter("b", 2)).andExpect(message("response-context-xslt.xml").withParameter("result", 3));
 	}
-//	
-//	@Test
-//	public void testAssertXPathContext() throws Exception {
-//		WsTestContextHolder.getTestContext().setAttribute("a", 1);
-//		WsTestContextHolder.getTestContext().setAttribute("b", 2);
-//		WsTestContextHolder.getTestContext().setAttribute("result", 3);
-//		// simulates request coming to MessageDispatcherServlet
-//		MessageContext message = helper.receiveMessage("request-context-xslt.xml");
-//		// evaluate XPath
-//		helper.createMessageValidator(message.getResponse()).assertNotSoapFault().validate("xsd/calc.xsd").addNamespaceMapping("ns", "http://javacrumbs.net/calc").assertXPath("//ns:result=$context.result");
-//		
-//		WsTestContextHolder.getTestContext().clear();
-//	}
-//	
-//	@Test
-//	public void testRequestTemplate() throws Exception {
-//		WsTestContextHolder.getTestContext().setAttribute("a", 1);
-//		WsTestContextHolder.getTestContext().setAttribute("b", 2);
-//		// simulates request coming to MessageDispatcherServlet
-//		MessageContext message = helper.receiveMessage("request-context-xslt.xml");
-//		// compare response with control response
-//		helper.createMessageValidator(message.getResponse()).assertNotSoapFault().compare("response1.xml");
-//	}
 }
