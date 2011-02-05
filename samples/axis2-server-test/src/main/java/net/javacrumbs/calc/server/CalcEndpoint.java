9744e2ea7c693e7e6371886a31ef31b61ef45ab1
Lukas Krecan <lukas@krecan.net>
2011-02-05 20:07:09 +0100

    Server working

--- samples/axis2-server-test/src/main/java/net/javacrumbs/calc/server/CalcEndpoint.java
+++ samples/axis2-server-test/src/main/java/net/javacrumbs/calc/server/CalcEndpoint.java
@@ -15,19 +15,8 @@
  */
 package net.javacrumbs.calc.server;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
-import javax.jws.WebService;
-
-@WebService(serviceName="CalculatorService", targetNamespace="http://javacrumbs.net/calc")
 public class CalcEndpoint {
-
-	private static final String NS = "http://javacrumbs.net/calc";
-
-	@WebMethod
-	@WebResult(name="result",targetNamespace=NS)
-	public long plus(@WebParam(name="a",targetNamespace=NS) long a, @WebParam(name="b",targetNamespace=NS) long b)
+	public long plus(long a, long b)
 	{
 		if (a+b>Integer.MAX_VALUE)
 		{
